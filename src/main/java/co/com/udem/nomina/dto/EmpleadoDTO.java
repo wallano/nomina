0611c84cae6b5879a52502fb7ea536b67549f91c
Walter Llano <walterllano@Walters-MacBook-Air.local>
2020-04-18 09:33:15 -0500

    Setup inicial del proyecto y DTO de Empleado

---
+++ src/main/java/co/com/udem/nomina/dto/EmpleadoDTO.java
@@ -0,0 +1,51 @@
+package co.com.udem.nomina.dto;
+
+public class EmpleadoDTO {
+
+	private String nombres;
+	private String apellidos;
+	private String cedula;
+	private String departamento;
+	private Double salario;
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+}
